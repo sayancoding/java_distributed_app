954e0d0fd5c8157d5348adece4a42648f3681a4b
sayancoding <sayanmaity007@gmail.com>
2023-05-05 10:46:44 +0530

    ProductDetail add

---
+++ product-service/src/main/java/com/arrowsModule/productservice/model/ProductDetail.java
@@ -0,0 +1,13 @@
+package com.arrowsModule.productservice.model;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class ProductDetail {
+    private String productId;
+    private String category;
+
+    private String shortNote;
+}
